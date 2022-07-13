f9293ac9f2308a60e663621207b6012c03786ff8
NejinPreena <108547108+NejinPreena@users.noreply.github.com>
2022-07-13 15:01:48 -0400

    js const Letters
    Ajout des lettres pour le jeu

--- www/javascript/script.js
+++ www/javascript/script.js
@@ -11,6 +11,8 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const isDebug = urlParams.get('debug');
 
+const Letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 let debug = (window.location.protocol == "file:") || (window.location.hostname == "127.0.0.1") || (isDebug == "true");
 if (isDebug == "false") {
     debug = false;
